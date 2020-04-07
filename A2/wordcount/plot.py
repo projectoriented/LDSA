9e242e419fd3dfdfee4d1eaa806a37cbcdb0da74
Mei Wu <savantmei@gmail.com>
2020-04-07 08:18:47 +0000

    deleted plot script

--- A2/wordcount/plot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3.6
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("part-r-00000", sep="\t", header=None, names=["alphabet","freq"])
-data.plot(kind='bar', y='freq', x='alphabet')
-plt.savefig('output.png')
