c19aa62a7c2b4a363c6e770bfc0d9e433039aec2
Mei Wu <savantmei@gmail.com>
2020-04-15 08:50:21 +0000

    DONE

--- A2/tweets_analysis/db-access.py
+++ A2/tweets_analysis/db-access.py
@@ -3,28 +3,11 @@
 from pymongo import MongoClient
 import re
 from bson.code import Code
-import pprint
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['twitter']
 collection_tweets = db.collection_tweets
 unique_tweet_count = db.collection_tweets.count() # fetch total doc. count in the collection
-
-#pronouns = ["han","hon","den","det","denna","denne","hen"] # pronouns to search for in each tweet
-#pronouns.sort()
-
-"""
-for x in pronouns:
-    counter = 0
-    regrex = re.compile(r"[\W \w]?\b" + x + r"\b[\W \w]?", re.IGNORECASE) # powerful regrex to match any special characters/alphabets/numbers flanking the pronouns
-
-    # Use regrex on MongoDB's find() method and process it further with a loop to get all matches within a tweet.
-    for posts in collection_tweets.find({'tweet': {'$regex': regrex }}):
-        counter += len(regrex.findall(posts['tweet']))
-    print('{}\t{}'.format(x, counter))
-
-print('unique_tweet_count\t{}'.format(unique_tweet_count))
-"""
 
 mapper = Code("""
                 function () {
@@ -52,9 +35,6 @@ reducer = Code("""
                     return total;
                 }
                 """)
-
-
-
 
 result = collection_tweets.map_reduce(mapper, reducer, "pronouns")
 for doc in result.find():
